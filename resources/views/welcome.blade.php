a3089d36a9475a408aa248274986288343f8b2e3
ShyD0s <sebastianarcos@unicauca.edu.co>
2026-08-13 16:56:59 -0500

    Change favicon link to local source
    Updated favicon link to use local icon file.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" type="image/png" href="https://www.sena.edu.co/Style%20Library/alayout/images/logoSena.png?rev=40">
+        <link rel="icon" type="image/png" src="/Icons/logoSena.png">
 
         <title>Sena Access</title>
 
